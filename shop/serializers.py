14cb4c0505519abed1ac0b0dc7b54dc33756bdf8
Oliver Haynes <olihaynes.dev@gmail.com>
2025-02-14 00:08:44 -0500

    Exposed stock quantity on fetched products.

--- shop/serializers.py
+++ shop/serializers.py
@@ -2,17 +2,26 @@ from rest_framework import serializers
 from shop.models import Category, Inventory, Product
 
 
+class InventorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inventory
+        fields = ['product', 'quantity_in_stock']
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['name', 'description']
 
 class ProductSerializer(serializers.ModelSerializer):
+    quantity_in_stock = serializers.SerializerMethodField()
+    
     class Meta:
         model = Product
-        fields = ['name', 'description', 'price', 'category', 'image_url']
+        fields = ['name', 'description', 'price', 'category', 'image_url', 'quantity_in_stock']
 
-class InventorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Inventory
-        fields = ['product', 'quantity_in_stock']
+    def get_quantity_in_stock(self, obj):
+        """Fetch inventory quantity for the given product."""
+        try:
+            return obj.inventory.quantity_in_stock
+        except Inventory.DoesNotExist:
+            return 0
