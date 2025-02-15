fd9554a19c98aa27517ee40e01cc71c461af8a3f
Oliver Haynes <olihaynes.dev@gmail.com>
2025-02-14 21:50:21 -0500

    Remove hardcoded secret key from settings for improved security.

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qnqs&)svcl&u%n=t*xvg61%76isullb(d&u3o!4b-t&2*1o!fi"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
